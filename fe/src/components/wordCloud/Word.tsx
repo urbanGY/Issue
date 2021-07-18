e56c47c69f12ddc53413c8c7773b746d3f4b9389
urbanGY <sfsfkj@gmail.com>
2021-07-18 23:54:40 +0900

    [#2] 기본 컴포넌트, 스타일 생성

--- fe/src/components/wordCloud/Word.tsx
+++ fe/src/components/wordCloud/Word.tsx
@@ -1,12 +1,24 @@
 import { observer } from 'mobx-react'
-import { Issue } from '$types/issue'
+import { wordStyle } from '$types/issue'
+import classNames from 'classnames/bind'
+import styles from './Word.module.scss'
+import { IssueModel } from '$stores/issueStore'
 
+const cx = classNames.bind(styles)
 interface WordInterface {
-    elem: Issue
+    elem: IssueModel
+    wordStyle: wordStyle
+    idx: number
 }
 
-const Word = observer(({ elem }: WordInterface) => {
-    return <p>{elem.value}</p>
+const Word = observer(({ elem, wordStyle, idx }: WordInterface) => {
+    return (
+        <>
+            <span className={cx('wrapper')} style={wordStyle}>
+                {elem.word}
+            </span>
+        </>
+    )
 })
 
 export default Word
--- fe/src/components/wordCloud/index.tsx
+++ fe/src/components/wordCloud/index.tsx
@@ -1,8 +1,13 @@
 import React, { useEffect } from 'react'
 import { observer } from 'mobx-react'
-import { getIssueStore } from '$stores/issueStore'
-
+import { getIssueStore, IssueModel } from '$stores/issueStore'
 import Word from './Word'
+import classNames from 'classnames/bind'
+import styles from './index.module.scss'
+
+import { randomRGB } from '$utils/common'
+
+const cx = classNames.bind(styles)
 
 const WordClound = observer(() => {
     const { issueList, fetchIssue } = getIssueStore()
@@ -15,12 +20,28 @@ const WordClound = observer(() => {
         return null
     }
 
+    const getWordStyle = (elem: IssueModel, idx: number) => {
+        const fontSize = 12 + elem.ratio * 5 + 'px'
+        const color = randomRGB()
+        return {
+            fontSize: fontSize,
+            color: color,
+        }
+    }
+
     return (
-        <>
+        <div className={cx('article')}>
             {issueList.map((elem, idx) => {
-                return <Word key={idx} elem={elem} />
+                return (
+                    <Word
+                        key={`${elem.word}_${idx}`}
+                        elem={elem}
+                        wordStyle={getWordStyle(elem, idx)}
+                        idx={idx}
+                    />
+                )
             })}
-        </>
+        </div>
     )
 })
 
